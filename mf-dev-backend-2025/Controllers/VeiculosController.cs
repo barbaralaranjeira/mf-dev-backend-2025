790a7057917172a8f096af7f4d04b7ddc7cda3ea
barbaralaranjeira <laranjeirabarbara@gmail.com>
2025-04-16 13:51:07 -0300

    Finalização das funcionalidades

--- mf-dev-backend-2025/Controllers/VeiculosController.cs
+++ mf-dev-backend-2025/Controllers/VeiculosController.cs
@@ -1,10 +1,12 @@
 ﻿using mf_dev_backend_2025.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 
 namespace mf_dev_backend_2025.Controllers
 {
+    [Authorize]
     public class VeiculosController : Controller
 
     {
@@ -104,5 +106,29 @@ namespace mf_dev_backend_2025.Controllers
 
             return RedirectToAction("Index");
         }
+
+        public async Task<IActionResult> Relatorio(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var veiculo = await _context.veiculos.FindAsync(id);
+
+            if (veiculo == null)
+                return NotFound();
+
+            var consumos = await _context.Consumos
+                .Where(c => c.VeiculoId == id)
+                .OrderByDescending(c => c.Data)
+                .ToListAsync(); 
+
+            decimal total = consumos.Sum(c => c.Valor);
+
+            ViewBag.Veiculo = veiculo;
+            ViewBag.Total = total;
+
+            return View(consumos);
+        }
+
     }
 }
